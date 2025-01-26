f6fc9d421cf90a64338735643df1e921834482bc
RenoirTan <renoirtan2005@gmail.com>
2025-01-26 13:12:01 +0800

    main: preorder iterator

--- src/binarytree.ts
+++ src/binarytree.ts
@@ -6,4 +6,30 @@ export default class BinaryTree<T> {
   constructor(root?: Node<T>) {
     this.root = root;
   }
+
+  preorderNodes(): Generator<Node<T>> {
+    return preorderNodesOf(this);
+  }
+
+  preorderValues(): Generator<T> {
+    return preorderValuesOf(this);
+  }
+}
+
+function* preorderNodesOf<T>(tree: BinaryTree<T>): Generator<Node<T>> {
+  if (!tree.root)
+    return;
+  const dfs: Node<T>[] = [tree.root];
+  while (dfs.length >= 1) {
+    const node = dfs.splice(0, 1)[0];
+    yield node;
+    if (node.right) dfs.push(node.right);
+    if (node.left) dfs.push(node.left);
+  }
+}
+
+function* preorderValuesOf<T>(tree: BinaryTree<T>): Generator<T> {
+  for (const node of preorderNodesOf(tree)) {
+    yield node.value;
+  }
 }
